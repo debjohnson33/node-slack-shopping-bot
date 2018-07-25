c4ea6369fc1bebf25964237059da0f79c4b5d882
debjohnson33 <debjohnson_11@yahoo.com>
2018-07-25 17:00:32 -0500

    Changed readFileSync to readFile

--- watson-discovery.js
+++ watson-discovery.js
@@ -1,5 +1,6 @@
 var DiscoveryV1 = require('watson-developer-cloud/discovery/v1');
 var fs = require('fs');
+require('dotenv').config()
 
 var discovery = new DiscoveryV1({
   username: process.env.DISCOVERY_USERNAME, //|| '<discovery_username>'
@@ -14,10 +15,16 @@ var discovery = new DiscoveryV1({
 //     console.log(JSON.stringify(data, null, 2));
 // };
 
-var file = fs.readFileSync('./data.json');
+var file = fs.readFile('./data.json', 'utf8', function(err, data) {  
+  if (err) throw err;
+  console.log(data);
+});
 
-discovery.addDocument(
-    { environment_id: process.env.DISCOVERY_ENVIRONMENT_ID, collection_id: process.env.DISCOVERY_COLLECTION_ID, file: file }),
+discovery.addDocument({ 
+  environment_id: process.env.DISCOVERY_ENVIRONMENT_ID, 
+  collection_id: process.env.DISCOVERY_COLLECTION_ID, 
+  file: file }),
 function(error, data) {
   console.log(JSON.stringify(data, null, 2));
+ console.log('Made it here');
 };
