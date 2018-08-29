f732f15c26f6c74e962cc5498d7f9cc6b7a4b8c3
debjohnson33 <debjohnson_11@yahoo.com>
2018-08-29 14:51:38 -0500

    Commented on async test prerequisites

--- test/async.test.js
+++ test/async.test.js
@@ -3,6 +3,8 @@ var asyncFetchData = require('../async');
 var chaiAsPromised = require('chai-as-promised');
 chai.use(chaiAsPromised).should();
 
+// Should have local server running at localhost:3001
+// Or a live website to retrieve products from
 describe('asyncFetchData', function() {
     it('should fulfill its promise', function(){
         return asyncFetchData().should.eventually.be.fulfilled
